e3dc986ed528efb5a6e1037a700ce907c384cada
yshanukajay <yshanukajay@gmail.com>
2026-01-07 13:07:17 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Kidney_disease_classification_using_MLFlow-DVC"
+AUTHOR_USER_NAME = "yshanukajay"
+SRC_REPO = "kidneyProject"
+AUTHOR_EMAIL = "yshanuka123@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
